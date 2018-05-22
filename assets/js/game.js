dac3c7d5ed4f1d5395b9663ff1b40482d59bb6b2
Katie <email@example.com>
2018-05-21 19:26:32 -0500

    reset btn resets the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,29 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
+	function reload() {
+		location.reload();
+	}
+
+
 	var resetCrystals = function(){
 		userScore = 0;
-		target = 0;
+		target= 0;
+		
 	}
+
+	//starts the game
 	var startGame = function(){
-		target = 0;
+		target =0;
 		userScore = 0;
 		
 		
-		$('#targetNumber').text(target);
+		 $('#targetNumber').text(target);
 	
 	var target = Math.floor(Math.random() * (100 + 19) +19 );
-	console.log(target);
-					 //doesn't work until I call it and put the min=19 and max=120
-		//I am lost as to what to do to get this attached to the buttons.			
+		
 	var crystal1 = Math.floor(Math.random() * (12 - 1)) + 1;
-	
 	var crystal2 = Math.floor(Math.random() * (12 - 1)) + 1;
-	
 	var crystal3 = Math.floor(Math.random() * (12 - 1)) + 1;
-	
 	var crystal4 = Math.floor(Math.random() * (12 - 1)) + 1;
 	
 	
@@ -47,25 +51,55 @@ $(document).ready(function () {
 	});
 	
 	
-	//---------------TONIGHT----------------------------------------------------------
-	//_______________if I have time learn to group fxns-----------
-	// var updateScore = function (){
-	// 	$(this).val();  //google userScore += num
-		//in each block of code userScore += the crystal1/2/3/4/
 	
 	function winCheck(){
 		$("#userSum").text(userScore);
-		if (userScore > target) {
-		$("#result").text("Aw, shoot!")
-		$(":reset").css("background-color", "red");
-		} else if (userScore === target) {
-			$("#result").text("you win") 
-			$(":reset").css("background-color", "red");
-		document.reset();
+		
+			if (userScore === target) {
+				console.log("won");
+				alert("You Won!");
+				reload();
+				
+				
+
+				
+
+			// swal({
+			// 	title:"You won!",
+			// 	text: "Click Ok to play again",
+			// 	icon: "success",
+			// 	button:"OK!",
+			// })
+		} else {
+			if (userScore > target){
+				console.log("lost");
+				alert("You lost.");
+				reload(); 
+				
+			// 	swal({
+			// 	title:"You lost.",
+			// 	text: "Click Ok to play again",
+			// 	icon:"error",
+			// 	button:"OK!",
+			// })
+				
+			};
+
 		}
 	}
-	}
+}
+
+
+
+	// 	if (userScore > target) {
+	// 	$("#result").text("Aw, shoot!")
+	// 	$(":reset").css("background-color", "red");
+	// 	} else if (userScore === target) {
+	// 		$("#result").text("you win") 
+	// 		$(":reset").css("background-color", "red");
+		
+	// 	}
+	
 	startGame();
-	});
 	
-	
+});
